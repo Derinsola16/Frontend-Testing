57fa1978e617d306b189a3c662913a9ad4a12eed
derinsola16 <aderinsolaodusanya@gmail.com>
2020-03-09 13:29:51 +0100

    Change variable number

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,5 @@
 const sum = require('./sum');
 
-test('adds 15 + 5 to equal 20', () => {
-  expect(sum(15, 5)).toBe(20);
+test('adds 10 + 5 to equal 20', () => {
+  expect(sum(10, 5)).toBe(20);
 });
